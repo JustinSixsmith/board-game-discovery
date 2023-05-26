df402a7d64c4ed6f6428f4a0b4a9d4aa2be8d724
Justin Sixsmith <justinsixsmith@gmail.com>
2023-05-25 23:05:49 -0500

    Displays all the videos in the GameVideo component

--- src/components/GameVideo.tsx
+++ src/components/GameVideo.tsx
@@ -1,3 +1,4 @@
+import { Box, Grid } from "@chakra-ui/react";
 import { VideosResponse } from "../entities/Video";
 import useVideos from "../hooks/useVideos";
 
@@ -11,24 +12,60 @@ const GameVideo = ({ gameId }: Props) => {
   if (isLoading) return null;
   if (error) throw error;
 
-  // We use type assertion here to specify that data is a VideosResponse
   const videosResponse = data as unknown as VideosResponse;
+  const videos = videosResponse?.videos;
 
-  const firstVideo = videosResponse?.videos[0];
-  const videoSrc = `https://www.youtube.com/embed/${getVideoId(
-    firstVideo?.url || ""
-  )}`;
-
-  return firstVideo ? (
-    <iframe
-      width="100%"
-      height="1000"
-      src={videoSrc}
-      title={firstVideo.title}
-      allowFullScreen
-    />
-  ) : null;
+  return (
+    <Grid
+      templateColumns={{ base: "1fr", md: "1fr 1fr", lg: "1fr 1fr 1fr 1fr" }}
+      gap={6}
+    >
+      {videos.map((video) => (
+        <Box
+          key={video.id}
+          position="relative"
+          pt="56.25%"
+          width="100%"
+          height="0"
+        >
+          <iframe
+            style={{
+              position: "absolute",
+              top: "0",
+              left: "0",
+              width: "100%",
+              height: "100%",
+            }}
+            src={`https://www.youtube.com/embed/${getVideoId(video.url)}`}
+            title={video.title}
+            allowFullScreen
+          />
+        </Box>
+      ))}
+    </Grid>
+  );
 };
+
+// const GameVideo = ({ gameId }: Props) => {
+//   const { data, error, isLoading } = useVideos(gameId);
+
+//   if (isLoading) return null;
+//   if (error) throw error;
+
+//   // Uses type assertion to specify that data is a VideosResponse
+//   const videosResponse = data as unknown as VideosResponse;
+
+//   const firstVideo = videosResponse?.videos[0];
+//   const videoSrc = `https://www.youtube.com/embed/${getVideoId(
+//     firstVideo?.url || ""
+//   )}`;
+
+//   return firstVideo ? (
+//     <div className="video-container">
+//       <iframe src={videoSrc} title={firstVideo.title} allowFullScreen />
+//     </div>
+//   ) : null;
+// };
 
 // Function to extract video id from YouTube URL
 function getVideoId(url: string): string {
